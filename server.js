2da6a432ee18e1f8157731340f5f89435b2c308a
krishna bihari rajpt <erkbrajput@gmail.com>
2019-02-11 22:10:22 +0530

    my commit

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
-
+const request = require("request");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
-
+var http = require("http");
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -42,5 +42,6 @@ if ((process.env.NODE_ENV = "production")) {
   });
 }
 const port = process.env.PORT || 5000;
+const app1 = http.createServer(request);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
